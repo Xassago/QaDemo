0ae7916d1be0ca98a87659a238db3e650fcf1911
L <181250089@smail.nju.edu.cn>
2022-02-07 10:07:38 +0800

    feat:ver1.0

---
+++ src/main/java/QaService.java
@@ -0,0 +1,8 @@
+public class QaService {
+    //调用QuestionProcessor做分词和数据处理 得到向量和所需数据
+    //将向量传递给ClassifyModel得到分类结果
+    //返回分类结果
+    public String qaService(String question){
+        return null;
+    }
+}
---
+++ src/main/java/QaStarter.java
@@ -0,0 +1,5 @@
+public class QaStarter {
+    public static void main(String[] args) {
+        //从控制台输入问句 调用QaService
+    }
+}
